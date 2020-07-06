81214139d641445c2581dd46bcb0edc28b370243
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-07-05 20:49:11 -0600

    provide RecipeService in recipes.component
    migrate recipes array from recipe-list.component to recipe.service;
    tap recipes array OnInit inside recipe-list.component via RecipeService's getRecipes()

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,18 +1,22 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 
-export class RecipeListComponent {
-  recipes: Recipe[] = [
-    new Recipe("Porkchop Fantasy", "The porkchop of your wildest dreams", "https://c1.wallpaperflare.com/preview/735/448/1005/food-meal-steak-pork-chop.jpg")
-  ];
+export class RecipeListComponent implements OnInit {
   @Output() recipePopulated = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   selectRecipe(recipeToDisplay: Recipe) {
     this.recipePopulated.emit(recipeToDisplay);
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,11 @@
+import { Recipe } from './recipe.model';
+
 export class RecipeService {
-  
+  private recipes: Recipe[] = [
+    new Recipe("Porkchop Fantasy", "The porkchop of your wildest dreams", "https://c1.wallpaperflare.com/preview/735/448/1005/food-meal-steak-pork-chop.jpg")
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
