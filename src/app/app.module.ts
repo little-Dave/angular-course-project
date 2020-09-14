928e97567642311689d384eced78f462f21780f9
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-09-13 20:35:57 -0600

    create recipe-edit component and associated files
    register in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 
 
@@ -28,7 +29,8 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
     ShoppingListComponent,
     ShoppingListEditComponent,
     DropdownDirective,
-    RecipeStartComponent
+    RecipeStartComponent,
+    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'recipe-edit',
+  templateUrl: 'recipe-edit.component.html',
+  styleUrls: ['recipe-edit.component.css']
+})
+
+export class RecipeEditComponent {
+
+}
